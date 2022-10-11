6c8d9937b1f99bb1b02777d35ac1459695ea0c9a
DevalapalleJaswanth <83700845+DevalapalleJaswanth@users.noreply.github.com>
2022-10-11 21:51:11 +0530

    3 files modified

--- src/BookMark/BookMark.js
+++ src/BookMark/BookMark.js
@@ -1,7 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { MapContext } from '../Store';
 export default function BookMark() {
   const [maps, setMaps] = useContext(MapContext);
+  let x = 'Subway';
+  let y =
+    'https://datastudio.google.com/embed/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params={"ds2.name2":"' +
+    x +
+    '"}';
 
-  return <div>BookMark Page</div>;
+  return (
+    <div>
+      <iframe
+        width="600"
+        height="450"
+        src={y}
+        frameborder="0"
+        style={{ border: 0 }}
+        allowfullscreen
+      ></iframe>
+      BookMark Page
+      {maps &&
+        maps.map((value, i) => {
+          <div key={i}>
+            {url}
+            <iframe
+              width="600"
+              height="450"
+              src={
+                'https://datastudio.google.com/embed/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params={"ds2.name2":"' +
+                value +
+                '"}'
+              }
+              frameborder="0"
+              style={{ border: 0 }}
+              allowfullscreen
+            ></iframe>
+          </div>;
+        })}
+    </div>
+  );
 }
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { getRestaurantsData } from '../Services';
 export default function Home() {
-  const [restaurants, setRestaurants] = useState('');
+  const [restaurant, setRestaurant] = useState('');
   useEffect(() => {
     getRestaurantsData('subway')
       .then((restaurantData) => restaurantData.json())
       .then((restaurantData) => {
         console.log(restaurantData);
-        setRestaurants(restaurantData.records);
+        setRestaurant(restaurantData.records);
       });
   }, []);
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <iframe
+        width="600"
+        height="450"
+        src={
+          'https://datastudio.google.com/embed/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params={"ds2.name2":"' +
+          restaurant +
+          '"}'
+        }
+        frameborder="0"
+        style={{ border: 0 }}
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
